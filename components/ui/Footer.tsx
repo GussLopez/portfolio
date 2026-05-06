1893b621a992c76f034316d3a45dea3220108e4b
Gustavo <gustavolopez819@gmail.com>
2026-05-06 12:33:39 -0500

    MODIFIED footer & installed motion

--- components/ui/Footer.tsx
+++ components/ui/Footer.tsx
@@ -1,17 +1,18 @@
-
 export default function Footer() {
   const year = new Date().getFullYear();
   return (
-    <footer className="container flex justify-between max-w-4xl mx-auto px-8 py-4 mt-10">
-      <span
-        className="text-lg font-gilda font-medium tracking-wide"
-      >
-        Locked In.
-      </span>
+    <footer className="container max-w-4xl mx-auto px-8 py-4 pb-10 mt-10">
+      <div className="flex justify-between">
+        <span
+          className="text-lg font-gilda font-medium tracking-wide"
+        >
+          Locked In.
+        </span>
 
-      <span className="text-muted-foreground tracking-wider text-xs">
-        © {year} Gustavo López Alvarado
-      </span>
+        <span className="text-muted-foreground tracking-wider text-xs">
+          © {year} Gustavo López Alvarado
+        </span>
+      </div>
     </footer>
   )
 }
