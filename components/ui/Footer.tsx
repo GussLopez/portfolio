77b266d26a1e215dda91c29218da52aab7aada76
Gustavo <gustavolopez819@gmail.com>
2026-05-06 16:42:09 -0500

    MODIFIED flex items center

--- components/ui/Footer.tsx
+++ components/ui/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   const year = new Date().getFullYear();
   return (
     <footer className="container max-w-4xl mx-auto px-8 py-4 pb-10 mt-10">
-      <div className="flex justify-between">
+      <div className="flex justify-between items-center">
         <span
           className="text-lg font-gilda font-medium tracking-wide"
         >
