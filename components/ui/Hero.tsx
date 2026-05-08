0c2e22bd866600a9f495039b25cf16667480a56b
Gustavo <gustavolopez819@gmail.com>
2026-05-07 21:43:26 -0500

    REMOVED fade up animation

--- components/ui/Hero.tsx
+++ components/ui/Hero.tsx
@@ -6,7 +6,7 @@ export default function Hero() {
   return (
     <section className="container max-w-4xl mx-auto px-8 py-4">
       <div>
-        <p className="opacity-80 delay-0 duration-500 animate-in fade-in slide-in-from-bottom-4 fill-mode-backwards leading-8 py-4 font-light">
+        <p className="opacity-80  leading-8 py-4 font-light">
           {t("im")} <span className="font-bold">Guss</span>, {t("currentStudy")} <a target="_blank" href={'https://utcancun.edu.mx'} className="pb-1 border-b border-transparent font-bold opacity-100 hover:border-foreground/60 transition-[border-color] duration-400">UT Cancún</a> {t("heroDescription")}
         </p>
       </div>
