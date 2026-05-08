8c533640ce238fc6f71ea92c57573a1fb0bc7ea9
Gustavo <gustavolopez819@gmail.com>
2026-05-08 02:10:14 -0500

    ADDED projects previews

--- components/ui/Proyects.tsx
+++ components/ui/Proyects.tsx
@@ -1,11 +1,89 @@
-import { useTranslation } from "react-i18next"
+import { ArrowRight, ArrowUpRight } from "lucide-react";
+import Link from "next/link";
+import { useTranslation } from "react-i18next";
+
+import { Separator } from "./separator";
+import { projects } from "@/data/projects";
+
+
 
 export default function Proyects() {
   const { t } = useTranslation();
 
   return (
-    <section className="container max-w-4xl mx-auto px-8 py-4 mt-10">
-      <h2 className="text-xl font-medium">{t("proyects")}</h2>
+    <section className="container max-w-3xl mx-auto px-8 py-4 mt-10">
+      <div className="flex justify-between items-center">
+        <h2 className="text-xl font-medium">{t("proyects")}</h2>
+
+        <Link
+          href="/projects"
+          className="flex items-center gap-1.5 text-sm opacity-70 hover:opacity-100 transition-all duration-300 group"
+        >
+          More
+
+          <ArrowRight className="size-3.5 group-hover:translate-x-0.5 transition-transform duration-300" />
+        </Link>
+      </div>
+
+      <div className="mt-6">
+        <Separator className="h-px w-full bg-muted" />
+
+        {projects.map((project) => (
+          <div key={project.title}>
+            <div className="py-4">
+              <div className="flex items-center justify-between">
+                <div className="lg:flex gap-5 items-center w-full">
+                  <Link
+                    href={project.href}
+                    target="_blank"
+                    className="flex items-center gap-1 group"
+                  >
+                    <h3 className="text-lg tracking-wide font-semibold truncate">
+                      {project.title}
+                    </h3>
+
+                    <ArrowUpRight className="size-3.5 opacity-70 group-hover:opacity-100 group-hover:translate-x-0.5 transition-transform duration-300" />
+                  </Link>
+
+                  <div className="flex justify-between items-center w-full mt-4 lg:mt-0">
+                    <div className="flex gap-2">
+                      {project.technologies.map((tech, index) => {
+                        const Icon = tech.icon;
+
+                        return (
+                          <span
+                            key={index}
+                            className="flex justify-center items-center w-8 h-8 border border-input rounded-md"
+                          >
+                            <Icon
+                              size={20}
+                              className={`size-5 ${tech.className}`}
+                            />
+                          </span>
+                        );
+                      })}
+                    </div>
+
+                    <Link
+                      href="#"
+                      className="p-2 border border-input rounded-md group"
+                    >
+                      <ArrowUpRight className="size-4 opacity-70 group-hover:opacity-100 transition-all" />
+                    </Link>
+                  </div>
+                </div>
+              </div>
+
+              <div className="lg:max-w-3/4 mt-4">
+                <p className="text-sm text-muted-foreground">
+                  {project.description}
+                </p>
+              </div>
+            </div>
+            <Separator className="h-px w-full bg-muted" />
+          </div>
+        ))}
+      </div>
     </section>
-  )
+  );
 }
---
+++ data/projects.ts
@@ -0,0 +1,87 @@
+import {
+  RiNextjsFill,
+  RiSupabaseFill,
+  RiTailwindCssFill,
+} from "react-icons/ri";
+import { SiExpress, SiShadcnui } from "react-icons/si";
+import { BiLogoPostgresql } from "react-icons/bi";
+
+export const projects = [
+  {
+    title: "Flyzzio",
+    href: "https://flyzzio.vercel.app/admin",
+    description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
+    technologies: [
+      {
+        icon: RiNextjsFill,
+        className: "",
+      },
+      {
+        icon: RiSupabaseFill,
+        className: "text-[#3FCF8E]",
+      },
+      {
+        icon: RiTailwindCssFill,
+        className: "text-[#06B6D4]",
+      },
+      {
+        icon: SiShadcnui,
+        className: "",
+      },
+    ],
+  },
+  {
+    title: "Hisgroup Company",
+    href: "https://hisgroupcompany.com",
+    description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
+    technologies: [
+      {
+        icon: RiNextjsFill,
+        className: "",
+      },
+      {
+        icon: SiExpress,
+        className: "",
+      },
+      {
+        icon: BiLogoPostgresql,
+        className: "text-[#4570f1]",
+      },
+      {
+        icon: RiTailwindCssFill,
+        className: "text-[#06B6D4]",
+      },
+      {
+        icon: SiShadcnui,
+        className: "",
+      },
+    ],
+  },
+  {
+    title: "Grupo Saimo",
+    href: "https://gruposaimo.com",
+    description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
+    technologies: [
+      {
+        icon: RiNextjsFill,
+        className: "",
+      },
+      {
+        icon: SiExpress,
+        className: "",
+      },
+      {
+        icon: BiLogoPostgresql,
+        className: "text-[#4570f1]",
+      },
+      {
+        icon: RiTailwindCssFill,
+        className: "text-[#06B6D4]",
+      },
+      {
+        icon: SiShadcnui,
+        className: "",
+      },
+    ],
+  },
+];
