d749132659634d55e7689ff7ca96aabe23355f02
Gustavo <gustavolopez819@gmail.com>
2026-05-12 13:10:38 -0500

    ADDED viewport height

--- app/stack/page.tsx
+++ app/stack/page.tsx
@@ -10,15 +10,17 @@ export const metadata: Metadata = {
 export default function StackPage() {
 
   return (
-    <main className="container max-w-3xl mx-auto px-8 py-8 grid grid-cols-1 gap-4">
-      <motion.h1
-        initial={{ opacity: 0, y: -6, filter: "blur(4px)" }}
-        animate={{ opacity: 1, y: 0, filter: "blur(0px)" }}
-        transition={{ duration: .3, ease: "easeIn" }}
-        className="article-heading tracking-[0.01em]"
-      >Mis stack y herramientas favoritas</motion.h1>
+    <main className="container max-w-3xl mx-auto px-8 py-8 min-h-[80vh]">
+      <div className="grid grid-cols-1 gap-4">
+        <motion.h1
+          initial={{ opacity: 0, y: -6, filter: "blur(4px)" }}
+          animate={{ opacity: 1, y: 0, filter: "blur(0px)" }}
+          transition={{ duration: .3, ease: "easeIn" }}
+          className="article-heading tracking-[0.01em]"
+        >Mis stack y herramientas favoritas</motion.h1>
 
-      <TechStack />
+        <TechStack />
+      </div>
     </main>
   )
 }
