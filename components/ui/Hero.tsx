4ce65d0170dc315e49d8d65a889adb92bfcb2264
Gustavo <gustavolopez819@gmail.com>
2026-07-18 13:28:05 -0500

    fix(hero): correct wording in introduction

--- components/ui/Hero.tsx
+++ components/ui/Hero.tsx
@@ -10,7 +10,7 @@ export default function Hero() {
           transition={{ duration: .3, ease: "easeIn" }}
           className="opacity-80 leading-8 py-4 font-light"
         >
-          I'm <span className="font-bold">Guss</span>, a Software Engineering student at <a target="_blank" href={'https://utcancun.edu.mx'} className="pb-1 border-b border-transparent font-bold opacity-100 hover:border-foreground/60 transition-[border-color] duration-400">Universidad Tecnológica de Cancún</a> and a full-stack developer based in Cancún, Mexico. With over a year of hands-on experience, I enjoy building modern web applications and crafting clean, user-friendly interfaces. I'm passionate about turning ideas into efficient, scalable, and intuitive digital solutions.
+          I'm <span className="font-bold">Guss</span>, a Software Engineering, studying at <a target="_blank" href={'https://utcancun.edu.mx'} className="pb-1 border-b border-transparent font-bold opacity-100 hover:border-foreground/60 transition-[border-color] duration-400">Universidad Tecnológica de Cancún</a> and a full-stack developer based in Cancún, Mexico. With over a year of hands-on experience, I enjoy building modern web applications and crafting clean, user-friendly interfaces. I'm passionate about turning ideas into efficient, scalable, and intuitive digital solutions.
         </motion.p>
       </div>
     </section>
