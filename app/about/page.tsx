e88a2dd8220ebdf4e5430a7d7e690d2f7ea1c22e
Gustavo <gustavolopez819@gmail.com>
2026-08-17 21:02:21 -0500

    feat(about): add entry motion animations

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,6 @@
+'use client'
+import { motion } from "motion/react";
+
 export default function AboutPage() {
 
   return (
@@ -5,31 +8,59 @@ export default function AboutPage() {
       <div className="absolute -top-45 right-40 w-70 h-80 blur-3xl rounded-full bg-brand"></div>
       <div className="absolute -top-45 right-65 w-70 h-70 blur-3xl rounded-full bg-orange-400"></div>
       <div>
-        <h1 className="text-6xl font-gilda">Gus López</h1>
-        <h2 className="text-6xl font-gilda">Student & Full Stack Developer</h2>
+        <motion.h1
+          className="text-6xl font-gilda"
+          initial={{ opacity: 0, y: 3 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: .3, delay: 0.1 }}
+        >Gus López</motion.h1>
+        <motion.h2
+          initial={{ opacity: 0, y: 3 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: .3, delay: 0.2 }}
+          className="text-6xl font-gilda"
+        >Student & Full Stack </motion.h2>
+        <motion.h2
+          initial={{ opacity: 0, y: 3 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: .3, delay: 0.3 }}
+          className="text-6xl font-gilda"
+        >Developer</motion.h2>
       </div>
       <div className="mt-14 space-y-6 text-muted-foreground font-light">
 
-        <p>I'm a <span className="font-bold text-white">Full Stack Software Developer</span>, currently studying <span>Software Engineering</span> at {" "}
-          <a target="_blank" href={'https://utcancun.edu.mx'} className="pb-1 border-b border-transparent font-bold hover:border-foreground/60 transition-all duration-400 hover:text-white">
+        <motion.p
+          initial={{ opacity: 0, y: -6, filter: "blur(4px)" }}
+          animate={{ opacity: 1, y: 0, filter: "blur(0px)" }}
+          transition={{ duration: .3, ease: 'easeIn' }}
+        >I'm a <span className="font-bold text-foreground">Full Stack Software Developer</span>, currently studying <span>Software Engineering</span> at {" "}
+          <a target="_blank" href={'https://utcancun.edu.mx'} className="pb-1 border-b border-transparent font-bold hover:border-foreground/60 transition-all duration-400 hover:text-foreground">
             Universidad Tecnológica de Cancún
           </a> {" "}
           where I'm in my final four-month term before starting my professional internship.
-        </p>
+        </motion.p>
 
-        <p>
+        <motion.p
+          initial={{ opacity: 0, y: -6, filter: "blur(4px)" }}
+          animate={{ opacity: 1, y: 0, filter: "blur(0px)" }}
+          transition={{ duration: .3, ease: 'easeIn' }}
+        >
           I've been working as a developer at {" "}
-          <a target="_blank" href={'https://hisgroupcompany.com'} className="pb-1 border-b border-transparent font-bold hover:border-foreground/60 transition-all duration-400 hover:text-white">
+          <a target="_blank" href={'https://hisgroupcompany.com'} className="pb-1 border-b border-transparent font-bold hover:border-foreground/60 transition-all duration-400 hover:text-foreground">
             Hisgroup Company
           </a>{" "}
           for over a year, where I develop and maintain commercial web applications, work across both frontend and backend, and contribute to building solutions that help the company grow.
-        </p>
+        </motion.p>
 
-        <p>I'm passionate about {" "}
-          <span className="font-bold text-white">
+        <motion.p
+          initial={{ opacity: 0, y: -6, filter: "blur(4px)" }}
+          animate={{ opacity: 1, y: 0, filter: "blur(0px)" }}
+          transition={{ duration: .3, ease: 'easeIn' }}
+        >I'm passionate about {" "}
+          <span className="font-bold text-foreground">
             building software that is useful, maintainable, and enjoyable to use.
           </span>{" "}
-          My main goal is to keep growing as a developer by learning new technologies, improving my problem-solving skills, and following good development practices.</p>
+          My main goal is to keep growing as a developer by learning new technologies, improving my problem-solving skills, and following good development practices.</motion.p>
       </div>
     </main>
   )
